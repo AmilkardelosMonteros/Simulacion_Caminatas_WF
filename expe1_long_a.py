433f40122426cd7d4ae57b3183cac4fffd38723c
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2020-04-04 22:36:52 -0600

    Update expe1_long_a.py

--- expe1_long_a.py
+++ expe1_long_a.py
@@ -3,23 +3,22 @@ import matplotlib.pyplot as plt
 from progress.bar import Bar, ChargingBar
 
 #u1 = 0.01 #Incremento en K(y)
-a1 = 0.5
-b1 = 50
-u1 = a1/b1#0.00980392156862745 #por que hicimos mas pequeño  el dominio
+ay = 0.5
+by = 50
+u1 = ay/by#0.00980392156862745 #por que hicimos mas pequeño  el dominio
 
-a2 = 1
-b2 = 50.0
-u2 = a2/b2#Incremento en S (x)
+ax = 1
+bx = 50.0
+u2 = ax/bx#Incremento en S (x)
 
 rep = 5000
 frec_inicial = 0.5
 x_init = 0
 y_init = 0.5
-nombre = 'Experimento1_longevidad_a_'+'frecuencia_'+str(frec_inicial)+'_u1_'+str(a1)+'%'+str(b1)+'_u2_'+str(a2)+'%'+ str(b2)+'.txt'
+nombre = 'Experimento1_longevidad_a_'+'frecuencia_'+str(frec_inicial)+'_u1_'+str(ay)+'%'+str(by)+'_u2_'+str(ax)+'%'+ str(bx)+'.txt'
 
 d0 = 1
 d1 = 1
-a0 = 0
 #archivo = open(nombre,'w')
 #Model M1
 def probafixWF(a,y): #para k = 0, (es un WF normal) a es la seleccion y y la frecuencia inicial
@@ -40,23 +39,24 @@ def probafixM1(a, k, y):
     return 1-p
 
 
-def probabilidades(s,a1):
+def probabilidades(s,a0):
     s_barra =  u2/(1+s)
     s_barra_neg  = -u2/(1+s)
+    a1 = a0 + u1
+    p_mas_mas = probafixM1(s_barra*((1+d1)/(1+d1+a1)),(a1*(1+d0) -a0*(1+d1))/((1+d1+a1)*(1+d1)),frec_inicial)
 
-    p_mas_mas = probafixM1(s_barra*((1+d1)/(1+d1+a1)),(a1*(1+d1) -a0*(1+d1))/((1+d1+a1)*(1+d1)),frec_inicial)
-    p_mas_menos = probafixM1(s_barra_neg*((1+d1)/(1+d1+a1)),(a1*(1+d1) -a0*(1+d1))/((1+d1+a1)*(1+d1)),frec_inicial)
+    p_mas_menos = probafixM1(s_barra_neg*((1+d1)/(1+d1+a1)),(a1*(1+d0) -a0*(1+d1))/((1+d1+a1)*(1+d1)),frec_inicial)
+    a1= a0 - u1
     p_menos_mas = 1 - probafixM1(s_barra_neg*((1+d0)/(1+d0+a0)),(a0*(1+d1) -a1*(1+d0))/((1+d0+a0)*(1+d0)),frec_inicial)
     p_menos_menos = 1 - probafixM1(s_barra*((1+d0)/(1+d0+a0)),(a0*(1+d1) -a1*(1+d0))/((1+d0+a0)*(1+d0)) ,frec_inicial)
-
     Sum  = p_mas_mas+p_mas_menos+p_menos_menos+p_menos_mas
     p_mas_mas =  p_mas_mas/Sum
     p_menos_mas =  p_menos_mas/Sum
     p_mas_menos =  p_mas_menos/Sum
     p_menos_menos =  p_menos_menos/Sum
     return [p_mas_mas,p_mas_menos,p_menos_mas,p_menos_menos]
-    
-    def caminata():
+
+def caminata():
     x = x_init
     y = y_init
     while True:
